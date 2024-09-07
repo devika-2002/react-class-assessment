7cd15472a68c42d4957ec7268a9a53c087736758
Devika Kumari <devika21@navgurukul.org>
2024-09-07 14:23:39 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  // const student = {
+  //     name: "nav",
+  //     age: "20",
+  //     class: "Programming"
+  // };
+
+  return (
+    <div>
+      navgurukul
+    </div>
+  );
+}
+
+export default App;
